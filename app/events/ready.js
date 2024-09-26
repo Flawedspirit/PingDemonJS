aa23c12040fd108ecdb2d900439f06497dba982a
Flawedspirit <flawedspirit@gmail.com>
2024-09-26 02:14:45 -0400

    Set a nice watching activity!

--- app/events/ready.js
+++ app/events/ready.js
@@ -1,9 +1,10 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        client.user.setActivity('Picarto and Piczel', { type: ActivityType.Watching });
         client.logger.log(`${client.user.tag} is now ready!`, 'green');
     }
 };
