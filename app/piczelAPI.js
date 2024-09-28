d0e571b27405c649a762a0a15675c9910f5bcb02
Flawedspirit <flawedspirit@gmail.com>
2024-09-28 01:24:46 -0400

    Added cache busting to API calls

--- app/piczelAPI.js
+++ app/piczelAPI.js
@@ -4,7 +4,7 @@ class PiczelAPI {
 
         // Sanitize input to only characters likely to be in a username
         const name = input.replace(/[^A-Za-zÀ-ÿ0-9_-]+/gi, "").trim();
-        const response = await fetch(APIUrl.concat("", name));
+        const response = await fetch(APIUrl.concat("", name), { headers: {'pragma': 'no-cache', 'cache-control': 'no-cache' }});
 
         return response.json();
     }
